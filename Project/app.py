ac3215e79dc835d259b3a46aeb2ce344197c2b90
nnmm794613 <65056620+nnmm794613@users.noreply.github.com>
2020-06-21 17:08:11 +0900

    프로젝트

--- Project/app.py
+++ Project/app.py
@@ -1,15 +1,36 @@
 import requests
 from flask import Flask
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)  
+db = client.dbsparta                      
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return 'This is Home!'
+
+# @app.route('/update_count', methods=['POST'])
+# def update_count():
+#     sentence = request.form['sentence']
+#     found_sentence = db.sentences.find_one({"sentence": sentence})
+#     if (found_sentence is None):
+#         db.sentences.insert_one({text: sentence, count: 1})
+#     else:
+#         db.sentences.update_one({"sentence": sentence}, {"$set": {count: found_sentence["count"] + 1}})
+
+@app.route('/')
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
 
 def get_phonetics(search_text):
-    # 타겟 URL을 읽어서 HTML를 받아오고,
+    
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get('https://www.macmillandictionary.com/dictionary/british/' + search_text,headers=headers)
 
-    # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
-    # soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
-    # 이제 코딩을 통해 필요한 부분을 추출하면 된다.   <span class="SEPPRON-before"> /</span>ˈwɔːtə(r)<span class="SEPPRON-after">/</span></span>'
+    
     soup = BeautifulSoup(data.text, 'html.parser')
 
     word = soup.select_one('#entryContent > div > div.col-xs-12.col-sm-8.col-md-content-with-right.left-content > div.middle-xs.entry-pron-head.row.no-margin > div.PRONS.dflex.no-grow.middle-xs > span.PRON', text=True)
