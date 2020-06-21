64bad5d5a74fd7f5f82cd6b3a341d20e890c9657
고지훈 <ujin43255252@gmail.com>
2020-06-21 17:17:28 +0900

    fixed phonetics api

--- Project/app.py
+++ Project/app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)  
@@ -20,11 +20,6 @@ def home():
 #     else:
 #         db.sentences.update_one({"sentence": sentence}, {"$set": {count: found_sentence["count"] + 1}})
 
-@app.route('/')
-
-if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
-
 def get_phonetics(search_text):
     
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -36,3 +31,12 @@ def get_phonetics(search_text):
     word = soup.select_one('#entryContent > div > div.col-xs-12.col-sm-8.col-md-content-with-right.left-content > div.middle-xs.entry-pron-head.row.no-margin > div.PRONS.dflex.no-grow.middle-xs > span.PRON', text=True)
 
     return word.text
+
+@app.route('/phonetics')
+def phonetics_get():
+    search_text = request.args.get('search_text')
+    phonetics = get_phonetics(search_text)
+    return jsonify({'result': 'success', 'phonetics': phonetics})
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
